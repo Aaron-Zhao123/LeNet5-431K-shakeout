696bedc34287fd68781d813c51c25104eee528c5
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-02 00:34:57 +0100

    try a differnet r1

--- run.py
+++ run.py
@@ -13,15 +13,19 @@ lr = 1e-4
 model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
 parent_dir = './'
 acc = 0
-save_name = 'cov0cov0fc0fc0'+'.pkl'
+# save_name = 'cov0cov0fc0fc0'+'.pkl'
 c_val_list = [0.5, 1, 5, 10]
 
 for c_val in c_val_list:
+    save_name = 'cval' + str( int (round(c_val * 10))) + '.pkl'
+    retrain = 0
     while (acc < 0.9936 and retrain < 2):
-        if (retrain == 2):
+        if (retrain == 1):
             lr = 1e-5
+            nopruning = False
         else:
             lr = 1e-4
+            nopruning = True
         param = [
         ('-pcov',pcov),
         ('-pcov2',pcov2),
@@ -34,7 +38,7 @@ for c_val in c_val_list:
         ('-weight_file_name', save_name),
         ('-shakeout_c', c_val),
         ('-parent_dir', parent_dir),
-        ('-nopruning', True)
+        ('-nopruning', nopruning)
         ]
         lr = lr / float(2)
         acc = training_shakeout.main(param)
