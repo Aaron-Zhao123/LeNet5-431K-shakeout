3eb0d173fab0825de1ea4d6405b59c34cbba6749
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-05-02 00:32:16 +0100

    try a differnet r1

--- run.py
+++ run.py
@@ -14,25 +14,31 @@ model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
 parent_dir = './'
 acc = 0
 save_name = 'cov0cov0fc0fc0'+'.pkl'
+c_val_list = [0.5, 1, 5, 10]
 
-while (acc < 0.9936 and retrain < 1):
-    param = [
-    ('-pcov',pcov),
-    ('-pcov2',pcov2),
-    ('-pfc',pfc),
-    ('-pfc2',pfc2),
-    ('-m',model_tag),
-    ('-lr',lr),
-    ('-dropout', 1.),
-    ('-train',True),
-    ('-weight_file_name', save_name),
-    ('-shakeout_c', 5.),
-    ('-parent_dir', parent_dir),
-    ('-nopruning', True)
-    ]
-    lr = lr / float(2)
-    acc = training_shakeout.main(param)
-    retrain = retrain + 1
+for c_val in c_val_list:
+    while (acc < 0.9936 and retrain < 2):
+        if (retrain == 2):
+            lr = 1e-5
+        else:
+            lr = 1e-4
+        param = [
+        ('-pcov',pcov),
+        ('-pcov2',pcov2),
+        ('-pfc',pfc),
+        ('-pfc2',pfc2),
+        ('-m',model_tag),
+        ('-lr',lr),
+        ('-dropout', 1.),
+        ('-train',True),
+        ('-weight_file_name', save_name),
+        ('-shakeout_c', c_val),
+        ('-parent_dir', parent_dir),
+        ('-nopruning', True)
+        ]
+        lr = lr / float(2)
+        acc = training_shakeout.main(param)
+        retrain = retrain + 1
 
 retrain = 0
 lr = 1e-4
